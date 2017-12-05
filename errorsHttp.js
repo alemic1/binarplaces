f0d2a4e0159fbc50cfbb4d4ac1b142b177b193bb
Aleksandra Mickiewicz <a.mickiewicz@binarapps.com>
2017-12-05 12:29:39 +0100

    add more messangers (403,422,500)

--- errorsHttp.js
+++ errorsHttp.js
@@ -2,6 +2,12 @@ function returnResponseForCode(response) {
   switch (response.status) {
     case 404:
       return 'Nie znaleziono';
+    case 403:
+      return 'Nie masz dostępu do tej funkcji';
+    case 422:
+      return 'Twoja sesja wygasła, zaloguj się ponownie';
+    case 500:
+      return 'Blad serwera';
     default:
       return 'Cos poszło nie tak';
   }

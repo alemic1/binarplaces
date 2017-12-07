b1034debcc93e788c72258ac24ace86bbcd74a34
alemic1 <a.mickiewicz@binarapps.com>
2017-12-07 15:58:27 +0100

    small changes with galery

--- galery.js
+++ galery.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   var $galery = $('.galery');
 
   $.each(restaurantsToShow, function(index, restaurant) {
-    $galery.append('<img src="./restauracja03.jpg" class="galeryImg">');
+    $galery.append(
+      '<a href="restauracja03.jpg" data-lightbox="image-1" data-title="RestaurantName"><img src="restauracja03.jpg" class="galeryImg"></a>'
+    );
   });
 });
